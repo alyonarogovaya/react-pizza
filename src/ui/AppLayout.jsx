cb5dfc1bc0f614401d76e96f1577d97d6179abfe
Alyona Rogovaya <alyona@yelk.com.ua>
2024-05-29 20:11:45 +0200

    Added styles to the app

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,23 +1,26 @@
 import Header from "./Header";
+import Spinner from "./Spinner";
 import CartOverview from "../features/cart/CartOverview";
 import { Outlet, useNavigation } from "react-router-dom";
-import Spinner from "./Spinner";
 
 function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
   return (
-    <>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Spinner />}
+
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
-    </>
+    </div>
   );
 }
 
