e80aabee740255482bb7ee20a56e24861083248a
Alyona Rogovaya <alyona@yelk.com.ua>
2024-06-01 15:53:54 +0200

    Inpmlemeted imitation of order process

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTotalPrice, getTotalQty } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const quantity = useSelector(getTotalQty);
-
   const totalPrice = useSelector(getTotalPrice);
+
+  if (quantity === 0) return;
 
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
@@ -13,7 +15,7 @@ function CartOverview() {
         <span>
           {quantity} {quantity > 1 ? "pizzas" : "pizza"}
         </span>
-        <span>${totalPrice}</span>
+        <span>{formatCurrency(totalPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
